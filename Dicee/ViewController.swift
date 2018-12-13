42a41fa1770372328fbbbd6deb931c7ac3d1c5fb
LinaChasova <a.chasova@innopolis.ru>
2018-12-13 11:29:16 +0300

    show random dices on loading the app

--- Dicee/ViewController.swift
+++ Dicee/ViewController.swift
@@ -19,6 +19,7 @@ class ViewController: UIViewController {
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view, typically from a nib.
+        updateDiceImages()
     }
 
     override func didReceiveMemoryWarning() {
@@ -27,15 +28,16 @@ class ViewController: UIViewController {
     }
 
     @IBAction func rullButtonPressed(_ sender: UIButton) {
-        
         // what should happen when roll button is pressed
-        
+        updateDiceImages()
+    }
+    
+    func updateDiceImages() {
         randomDiceIndex1 = Int(arc4random_uniform(6))
         randomDiceIndex2 = Int(arc4random_uniform(6))
         
         diceImageView1.image = UIImage(named: "dice\(randomDiceIndex1 + 1)")
         diceImageView2.image = UIImage(named: "dice\(randomDiceIndex2 + 1)")
-        
     }
     
 }
