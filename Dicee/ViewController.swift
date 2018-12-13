ef779d366d0958dd96d2958b6eeedc901b70d080
LinaChasova <a.chasova@innopolis.ru>
2018-12-13 11:00:17 +0300

    added simple method for roll button

--- Dicee/ViewController.swift
+++ Dicee/ViewController.swift
@@ -9,6 +9,9 @@
 import UIKit
 
 class ViewController: UIViewController {
+    
+    var randomDiceIndex1 : Int = 0
+    var randomDiceIndex2 : Int = 0
 
     @IBOutlet weak var diceImageView1: UIImageView!
     @IBOutlet weak var diceImageView2: UIImageView!
@@ -27,6 +30,10 @@ class ViewController: UIViewController {
         
         // what should happen when roll button is pressed
         
+        randomDiceIndex1 = Int(arc4random_uniform(5))
+        randomDiceIndex2 = Int(arc4random_uniform(5))
+        
+        print(randomDiceIndex1)
     }
     
 }
